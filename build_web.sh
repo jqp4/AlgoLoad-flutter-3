4897f1566428f9db6df55a43fc6cb3c74eb5b37d
Gleb Skryabin <skryabin.gleb.dev@gmail.com>
2025-04-05 14:59:28 +0300

    chore: add nginx configuration
    Deleted outdated documentation files, icons, and assets related to the Flutter web app. Added a new nginx configuration file and related logs to support the deployment setup. This cleanup ensures the repository only contains relevant and up-to-date files.

--- build_web.sh
+++
@@ -1,8 +0,0 @@
-flutter clean
-flutter build web --base-href="/AlgoLoad-flutter-3/"
-rm build/web/flutter_service_worker.js
-
-rm -rf docs
-mkdir docs
-cp -r build/web/* docs/
-cp docs/index.html docs/404.html
